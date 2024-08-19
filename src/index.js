58161daae8f792cafb004dd577bcd5c95dbcef7c
Yassin <55637659+IIyn@users.noreply.github.com>
2024-08-19 17:36:34 +0200

    chore : Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ dotenv.config();
 
 const { TOKEN } = process.env;
 
-const RESPONSE_LIMIT_TIME = 2000; // 2 seconds
-let lastResponseTimestamp = null;
-
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 refreshCommands(rest);
